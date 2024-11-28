0167dce7a81c09de6c7bb10a33e7b598f3045c7d
Dmitriy <popov.dmitriy.ivanovich@gmail.com>
2024-11-28 16:52:30 +0300

    Regen docs

--- server/docs/docs.go
+++ server/docs/docs.go
@@ -934,7 +934,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "83.69.248.180:8080",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "GenMilk API",
