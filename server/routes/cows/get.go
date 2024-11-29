3ba86b708fc0a5089fd413ce86d1930ca3692466
Dmitriy <popov.dmitriy.ivanovich@gmail.com>
2024-11-29 14:40:47 +0300

    Этот код можно будет детям показывать ... в цирке

--- server/routes/cows/get.go
+++ server/routes/cows/get.go
@@ -3,9 +3,39 @@ package cows
 import (
 	"cow_backend/models"
 	"cow_backend/routes"
+	"errors"
 
 	"github.com/gin-gonic/gin"
 )
+
+type ReserealizedCow struct {
+	models.Cow
+	BreedName *string `json:",omitempty"`
+	SexName   *string `json:",omitempty"`
+}
+
+func (rc ReserealizedCow) GetReserealizer() routes.Reserealizer {
+	return &rc
+}
+func (rc *ReserealizedCow) FromBaseModel(c any) (routes.Reserealizable, error) {
+	cow, ok := c.(models.Cow)
+	if !ok {
+		return ReserealizedCow{}, errors.New("wrong type provided to get new cow from db cow")
+	}
+	db := models.GetDb()
+	breed := models.Breed{}
+	sex := models.Sex{}
+	if err := db.First(&breed, cow.BreedId).Error; err != nil {
+		return ReserealizedCow{}, err
+	}
+	if err := db.First(&sex, cow.SexId).Error; err != nil {
+		return ReserealizedCow{}, err
+	}
+	rc.Cow = cow
+	rc.BreedName = &breed.Name
+	rc.SexName = &sex.Name
+	return *rc, nil
+}
 
 // ListAccounts lists all existing accounts
 //
@@ -20,5 +50,6 @@ import (
 //	@Failure      500  {object}  map[string]error
 //	@Router       /cows/get [get]
 func (f *Cows) Get() func(*gin.Context) {
-	return routes.GenerateGetFunction[models.Cow]("farm_id", "farm_group_id")
+	return routes.GenerateReserealizingGetFunction[models.Cow, ReserealizedCow]("farm_id", "farm_group_id")
+	// return routes.GenerateGetFunction[models.Cow]("farm_id", "farm_group_id")
 }
--- server/routes/general.go
+++ server/routes/general.go
@@ -40,3 +40,50 @@ func GenerateGetFunction[T any](filters ...string) func(c *gin.Context) {
 	}
 
 }
+
+type Reserealizer interface {
+	FromBaseModel(value any) (Reserealizable, error)
+}
+
+type Reserealizable interface {
+	GetReserealizer() Reserealizer
+}
+
+func GenerateReserealizingGetFunction[DbModel any, R Reserealizable](filters ...string) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		db := models.GetDb()
+		id, _ := c.GetQuery("id")
+		objs := []DbModel{}
+		query := db.Where("true")
+		if id != "" {
+			query = db.Where("id = ?", id)
+		}
+		for _, filter := range filters {
+			value, _ := c.GetQuery(filter)
+			if value == "" {
+				continue
+			}
+			query = query.Where(map[string]string{filter: value})
+		}
+		if err := query.Find(&objs, id).Error; err != nil {
+			c.JSON(500, err)
+			return
+		}
+		res := make([]R, len(objs))
+		for idx, obj := range objs {
+			reserealizer := res[idx].GetReserealizer()
+			if reserealized, err := reserealizer.FromBaseModel(obj); err != nil {
+				c.JSON(500, err)
+				return
+			} else {
+				if typedReserealized, ok := reserealized.(R); !ok {
+					c.JSON(500, gin.H{"error": "reserealizer returned wrong type"})
+				} else {
+					res[idx] = typedReserealized
+				}
+			}
+		}
+		c.JSON(200, res)
+	}
+
+}
