8ebfe530cc190c89c83a46e1bbafeb5adae59e91
Nero <doglermr9@gmail.com>
2024-12-05 23:13:25 +0300

    Add AdminUsersTable

--- server/routes/admin/admin.go
+++ server/routes/admin/admin.go
@@ -23,14 +23,17 @@ func (s *Admin) CheckCowTable() func(*gin.Context) {
 	return func(c *gin.Context) {
 		db := models.GetDb()
 		cows := []models.Cow{}
-		db.Find(&cows)
+		db.Where("approved = 0").Find(&cows)
 		c.HTML(http.StatusOK, "AdminCowTablePage.tmpl", gin.H{"title": "Таблица коров", "cows": cows})
 	}
 }
 
 func (s *Admin) CheckUsersTable() func(*gin.Context) {
 	return func(c *gin.Context) {
-		c.HTML(http.StatusOK, "AdminUsersPage.tmpl", gin.H{"title": "Таблица пользователей"})
+		db := models.GetDb()
+		users := []models.User{}
+		db.Find(&users)
+		c.HTML(http.StatusOK, "AdminUsersPage.tmpl", gin.H{"title": "Таблица пользователей", "users": users})
 	}
 }
 
