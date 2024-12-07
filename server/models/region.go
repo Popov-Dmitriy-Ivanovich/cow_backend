2de5f7d49689598eca3f29b477a03d6aab9e5b15
Popov Dmitriy Ivanovich <PopovDmitriyStudy@yandex.ru>
2024-12-07 23:26:30 +0300

    add region number

--- server/models/region.go
+++ server/models/region.go
@@ -4,4 +4,5 @@ type Region struct {
 	ID   uint   `gorm:"primaryKey" default:"1"`
 	Name string `example:"Усть-Каменский"`
 	News []News
+	RegNum uint // номер региона (Архангельская область = 29)
 }
