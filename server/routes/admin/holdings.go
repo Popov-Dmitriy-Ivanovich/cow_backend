4430b71c69ad6e71b80e2c0a32099e0335f5071f
Nero <doglermr9@gmail.com>
2024-12-08 17:17:30 +0300

    fix: holdings page

--- server/routes/admin/holdings.go
+++ server/routes/admin/holdings.go
@@ -2,7 +2,9 @@ package admin
 
 import (
 	"cow_backend/models"
+	"math"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,8 +12,28 @@ import (
 func (s *Admin) CheckHoldingsTable() func(*gin.Context) {
 	return func(c *gin.Context) {
 		db := models.GetDb()
+		pageStr := c.Query("page") // Get the requested page number from the query string.
+		page, err := strconv.Atoi(pageStr)
+		if err != nil {
+			page = 1
+		}
+		limit := 20
+		offset := (page - 1) * limit
+
 		holds := []models.Farm{}
-		db.Where("parrent_id is Null").Find(&holds)
-		c.HTML(http.StatusOK, "AdminHoldingsPage.tmpl", gin.H{"title": "Таблица холдингов", "holds": holds})
+		db.
+			Where("parrent_id is Null").
+			Limit(limit).
+			Offset(offset).
+			Find(&holds)
+
+		var count int64
+		db.Model(&models.Farm{}).Where("parrent_id is Null").Count(&count)
+		totalPages := int(math.Ceil(float64(count) / float64(limit)))
+		c.HTML(http.StatusOK, "AdminHoldingsPage.tmpl", gin.H{
+			"title":       "Таблица холдингов",
+			"holds":       holds,
+			"currentPage": page,
+			"totalPages":  totalPages})
 	}
 }
