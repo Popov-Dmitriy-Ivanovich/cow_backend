28b300e7ea6b22d3ebdd23ccc759abc6b668dc7a
Nero <doglermr9@gmail.com>
2024-12-08 16:27:19 +0300

    Add regions/{id}/getFarms

---
+++ server/routes/regions/frams.go
@@ -0,0 +1,36 @@
+package regions
+
+import (
+	"cow_backend/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+// ListAccounts lists all existing accounts
+//
+//	@Summary      Get farm by region id
+//	@Description  Возращает все фермы в регионе
+//
+// @Tags         Farms
+// @Param        id    path     int  true  "id of region"
+// @Produce      json
+// @Success      200  {object}   models.Farm
+// @Failure      500  {object}  map[string]error
+// @Router       regions/{id}/getFarms [get]
+func (f *Regions) GetFarms() func(*gin.Context) {
+	return func(c *gin.Context) {
+		id := c.Param("id")
+		db := models.GetDb()
+		farms := []models.Farm{}
+		dist := models.District{}
+		if err := db.First(&dist, id).Error; err != nil {
+			c.JSON(500, err.Error)
+			return
+		}
+		if err := db.Where("district_id = ?", dist.ID).Find(&farms).Error; err != nil {
+			c.JSON(500, err.Error)
+			return
+		}
+		c.JSON(200, gin.H{"farms": farms})
+	}
+}
