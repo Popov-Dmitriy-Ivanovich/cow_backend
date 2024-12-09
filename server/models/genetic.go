e6e8e89d697cc57a20cf74f4a3a32a766abd56d7
Popov Dmitriy Ivanovich <PopovDmitriyStudy@yandex.ru>
2024-12-09 13:56:26 +0300

    genetic parser

--- server/models/genetic.go
+++ server/models/genetic.go
@@ -4,9 +4,9 @@ type Genetic struct {
 	ID                        uint             `gorm:"primaryKey"` // ID записи о генотипировании
 	CowID                     uint             // ID коровы
 	ProbeNumber               string           // Номер пробы
-	BloodDate                 DateOnly         // Дата взятия пробы крови
-	ResultDate                DateOnly         // Дата получения  результата
-	InbrindingCoeffByGenotype float64          // Коэф. инбриндинга по генотипу
+	BloodDate                 *DateOnly         // Дата взятия пробы крови
+	ResultDate                *DateOnly         // Дата получения  результата
+	InbrindingCoeffByGenotype *float64          // Коэф. инбриндинга по генотипу
 	GeneticIllnesses          []GeneticIllness `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;many2many:genetic_genetic_illnesses;"` // Список генетических заболеваний, пустой если нет
 
 	GtcFilePath *string
@@ -15,5 +15,12 @@ type GeneticIllness struct {
 	ID          uint    `gorm:"primaryKey"`
 	Name        string  // имя генетического заболевания
 	Description string  // описание генетического заболевания
-	OMIA        *string // Какой-то там ОМИЯ номер
+	OMIA        *string // Какой-то там ОМИЯ номер	
+	Status 		*GeneticIllnessStatus // статус этой фигни (болен, не болен ...)
+	StatusID    *uint
+}
+
+type GeneticIllnessStatus struct {
+	ID uint `gorm:"primaryKey"`
+	Status string 
 }
