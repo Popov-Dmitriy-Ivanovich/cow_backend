6280ec695ebf266f639655aed1dd82a22e01112d
Dmitriy <popov.dmitriy.ivanovich@gmail.com>
2024-12-10 22:40:41 +0300

    I'm sad

--- server/routes/farms/farm.go
+++ server/routes/farms/farm.go
@@ -12,8 +12,8 @@ type Farms struct {
 func (f *Farms) WriteRoutes(rg *gin.RouterGroup) {
 	apiGroup := rg.Group("/farms")
 	apiGroup.GET("/:id", f.GetByID())
-	// apiGroup.GET("/", f.GetByFilter())
-	apiGroup.GET("/", func(c *gin.Context) {
+	apiGroup.GET("/", f.GetByFilter())
+	apiGroup.GET("/hoz", func(c *gin.Context) {
 		db := models.GetDb()
 		farms := []models.Farm{}
 		qres := db.Where("EXISTS (SELECT 1 FROM COWS WHERE cows.farm_group_id = farms.id)").Find(&farms)
