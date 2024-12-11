bd0f95cc0130fb3b404dff6545dd68d126817d5c
Dmitriy <popov.dmitriy.ivanovich@gmail.com>
2024-12-11 22:43:08 +0300

    fix

--- server/filters/cows_filter/by_any_illnesses.go
+++ server/filters/cows_filter/by_any_illnesses.go
@@ -24,9 +24,9 @@ func (f ByAnyIllneses) Apply(fm filters.FilteredModel) error {
 	}
 	if bodyData.HasAnyIllnes != nil && !*bodyData.HasAnyIllnes {
 		query = query.Where("EXISTS (SELECT 1 FROM genetics where genetics.cow_id = cows.id AND " +
-			"EXISTS (SELECT 1 FROM genetic_illness_data WHERE genetic_illness_data.genetic_id = genetics.id AND " +
+			"NOT EXISTS (SELECT 1 FROM genetic_illness_data WHERE genetic_illness_data.genetic_id = genetics.id AND " +
 			"EXISTS (SELECT 1 FROM genetic_illness_statuses WHERE genetic_illness_statuses.id = genetic_illness_data.status_id AND " +
-			"(genetic_illness_statuses.status IS NOT NULL or genetic_illness_statuses.status <> 'FREE'))))").
+			"(genetic_illness_statuses.status IS NOT NULL AND genetic_illness_statuses.status <> 'FREE'))))").
 			Preload("Genetic").Preload("Genetic.GeneticIllnessesData").Preload("Genetic.GeneticIllnessesData.Illness").
 			Preload("Genetic.GeneticIllnessesData.Status")
 	}
