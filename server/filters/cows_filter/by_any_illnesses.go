b5428132990805d0bd7d5e5d0b4f63845c59c7b1
Dmitriy <popov.dmitriy.ivanovich@gmail.com>
2024-12-11 22:25:21 +0300

    Fix

--- server/filters/cows_filter/by_any_illnesses.go
+++ server/filters/cows_filter/by_any_illnesses.go
@@ -15,7 +15,7 @@ func (f ByAnyIllneses) Apply(fm filters.FilteredModel) error {
 		return errors.New("wrong object provided in filter filed object")
 	}
 	if bodyData.HasAnyIllnes != nil && *bodyData.HasAnyIllnes {
-		query = query.Where("EXISTS (SELECT 1 FROM genetics where genetics.cow_id = cows.id AND" +
+		query = query.Where("EXISTS (SELECT 1 FROM genetics where genetics.cow_id = cows.id AND " +
 			"EXISTS (SELECT 1 FROM genetic_illness_data WHERE genetic_illness_data.genetic_id = genetics.id AND " +
 			"EXISTS (SELECT 1 FROM genetic_illness_statuses WHERE genetic_illness_statuses.id = genetic_illness_data.status_id AND " +
 			"(genetic_illness_statuses.status IS NOT NULL or genetic_illness_statuses.status <> 'FREE'))))").
@@ -23,7 +23,7 @@ func (f ByAnyIllneses) Apply(fm filters.FilteredModel) error {
 			Preload("Genetic.GeneticIllnessesData.Status")
 	}
 	if bodyData.HasAnyIllnes != nil && !*bodyData.HasAnyIllnes {
-		query = query.Where("EXISTS (SELECT 1 FROM genetics where genetics.cow_id = cows.id AND" +
+		query = query.Where("EXISTS (SELECT 1 FROM genetics where genetics.cow_id = cows.id AND " +
 			"EXISTS (SELECT 1 FROM genetic_illness_data WHERE genetic_illness_data.genetic_id = genetics.id AND " +
 			"NOT EXISTS (SELECT 1 FROM genetic_illness_statuses WHERE genetic_illness_statuses.id = genetic_illness_data.status_id AND " +
 			"(genetic_illness_statuses.status IS NOT NULL or genetic_illness_statuses.status <> 'FREE'))))").
