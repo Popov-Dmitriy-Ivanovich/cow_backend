5a238f75f201db29b5294916e19e1898498e1769
Popov Dmitriy Ivanovich <PopovDmitriyStudy@yandex.ru>
2024-12-13 23:11:33 +0300

    Fix filter by illness

--- server/filters/cows_filter/by_any_illnesses.go
+++ server/filters/cows_filter/by_any_illnesses.go
@@ -18,7 +18,7 @@ func (f ByAnyIllneses) Apply(fm filters.FilteredModel) error {
 		query = query.Where("EXISTS (SELECT 1 FROM genetics where genetics.cow_id = cows.id AND " +
 			"EXISTS (SELECT 1 FROM genetic_illness_data WHERE genetic_illness_data.genetic_id = genetics.id AND " +
 			"EXISTS (SELECT 1 FROM genetic_illness_statuses WHERE genetic_illness_statuses.id = genetic_illness_data.status_id AND " +
-			"(genetic_illness_statuses.status IS NOT NULL AND genetic_illness_statuses.status <> 'FREE'))))").
+			"(genetic_illness_statuses.status IS NULL OR genetic_illness_statuses.status <> 'FREE'))))").
 			Preload("Genetic").Preload("Genetic.GeneticIllnessesData").Preload("Genetic.GeneticIllnessesData.Illness").
 			Preload("Genetic.GeneticIllnessesData.Status")
 	}
@@ -26,7 +26,7 @@ func (f ByAnyIllneses) Apply(fm filters.FilteredModel) error {
 		query = query.Where("EXISTS (SELECT 1 FROM genetics where genetics.cow_id = cows.id AND " +
 			"NOT EXISTS (SELECT 1 FROM genetic_illness_data WHERE genetic_illness_data.genetic_id = genetics.id AND " +
 			"EXISTS (SELECT 1 FROM genetic_illness_statuses WHERE genetic_illness_statuses.id = genetic_illness_data.status_id AND " +
-			"(genetic_illness_statuses.status IS NOT NULL AND genetic_illness_statuses.status <> 'FREE'))))").
+			"(genetic_illness_statuses.status IS NULL OR genetic_illness_statuses.status <> 'FREE'))))").
 			Preload("Genetic").Preload("Genetic.GeneticIllnessesData").Preload("Genetic.GeneticIllnessesData.Illness").
 			Preload("Genetic.GeneticIllnessesData.Status")
 	}
