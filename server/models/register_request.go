a119d4fbc4c4ce5573c565ce2f5784b2c5126aaa
Popov Dmitriy Ivanovich <PopovDmitriyStudy@yandex.ru>
2024-12-14 18:31:33 +0300

    Add user confirm

---
+++ server/models/register_request.go
@@ -0,0 +1,44 @@
+package models
+
+type UserRegisterRequest struct {
+	ID 						uint
+	HozNumber 				string // номер хоз-ва к которому привязвыается пользователь: либо существует, либо newHoz
+	
+	NameSurnamePatronimic 	string
+	RoleId                	uint
+	Email                 	string
+	Phone                 	string
+	Password              	string
+	
+	RegionId              	uint
+}
+type HozRegisterRequest struct {
+	ID 				uint
+	
+	HoldNumber 		string
+
+	HozNumber   	string
+	DistrictId  	uint
+	
+	Name        	string
+	ShortName   	string
+	Inn         	*string
+	Address     	*string
+	Phone       	*string
+	Email       	*string
+	Description 	*string
+	CowsCount   	*uint
+}
+type HoldRegisterRequest struct {
+	ID 			uint
+	HozNumber   string
+	DistrictId  string
+	Name        string
+	ShortName   string
+	Inn         *string
+	Address     *string
+	Phone       *string
+	Email       *string
+	Description *string
+	CowsCount   *uint
+}
