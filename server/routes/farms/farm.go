b89317aab4b7dc6491533b5b7cc113cedfcd82f6
Nero <doglermr9@gmail.com>
2024-12-14 20:39:01 +0300

    Add mdlwr to hoz filter

--- server/routes/farms/farm.go
+++ server/routes/farms/farm.go
@@ -2,6 +2,8 @@ package farms
 
 import (
 	"cow_backend/models"
+	"cow_backend/routes/auth"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -13,13 +15,51 @@ func (f *Farms) WriteRoutes(rg *gin.RouterGroup) {
 	apiGroup := rg.Group("/farms")
 	apiGroup.GET("/:id", f.GetByID())
 	apiGroup.GET("/", f.GetByFilter())
-	apiGroup.GET("/hoz", func(c *gin.Context) {
+	authGroup := apiGroup.Group("")
+	authGroup.Use(auth.AuthMiddleware(auth.Farmer, auth.RegionalOff, auth.FederalOff))
+	authGroup.GET("/hoz", func(c *gin.Context) {
+		roleId, exists := c.Get("RoleId")
+		if !exists {
+			c.JSON(http.StatusInternalServerError, "RoleId не найден в контексте")
+			return
+		}
+
 		db := models.GetDb()
 		farms := []models.Farm{}
-		qres := db.Where("EXISTS (SELECT 1 FROM COWS WHERE cows.farm_group_id = farms.id)").Find(&farms)
-		if qres.Error != nil {
-			c.JSON(500, qres.Error)
+		if roleId == 1 {
+			farmId, exists := c.Get("FarmId")
+			if !exists {
+				c.JSON(http.StatusInternalServerError, "FarmId не найден в контексте")
+				return
+			}
+			qres := db.Where("EXISTS (SELECT 1 FROM COWS WHERE cows.farm_group_id = farms.id) AND id = ?", farmId).Find(&farms)
+			if qres.Error != nil {
+				c.JSON(500, qres.Error)
+			}
+
+			c.JSON(200, farms)
+		} else if roleId == 2 {
+			regionId, exists := c.Get("RegionId")
+			if !exists {
+				c.JSON(http.StatusInternalServerError, "RegionId не найден в контексте")
+				return
+			}
+			qres := db.
+				Joins("JOIN districts AS d ON farms.district_id = d.id").
+				Joins("JOIN regions AS r ON r.id = d.region_id").
+				Where("EXISTS (SELECT 1 FROM COWS WHERE cows.farm_group_id = farms.id) AND r.id = ?", regionId).
+				Find(&farms)
+			if qres.Error != nil {
+				c.JSON(500, qres.Error)
+			}
+			c.JSON(200, farms)
+		} else {
+			qres := db.Where("EXISTS (SELECT 1 FROM COWS WHERE cows.farm_group_id = farms.id)").Find(&farms)
+			if qres.Error != nil {
+				c.JSON(500, qres.Error)
+			}
+			c.JSON(200, farms)
 		}
-		c.JSON(200, farms)
+
 	})
 }
