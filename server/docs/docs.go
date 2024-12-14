47bea332c722fbe1ca7e9a3073567cc6be3e48ea
Popov Dmitriy Ivanovich <PopovDmitriyStudy@yandex.ru>
2024-12-14 18:38:19 +0300

    REGEN SWAGGER

--- server/docs/docs.go
+++ server/docs/docs.go
@@ -3428,7 +3428,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "genmilk.ru",
 	BasePath:         "/api",
 	Schemes:          []string{},
 	Title:            "GenMilk API",
