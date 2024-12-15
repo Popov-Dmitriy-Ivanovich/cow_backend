97ac06a9054e115577f183ecd98562092a0ac580
Popov Dmitriy Ivanovich <PopovDmitriyStudy@yandex.ru>
2024-12-15 15:59:37 +0300

    Сережа, что дима на вет. мероприятия выдаёт?@

--- server/routes/cows/health.go
+++ server/routes/cows/health.go
@@ -28,7 +28,7 @@ func (f *Cows) Health() func(*gin.Context) {
 		}
 
 		events := []models.Event{}
-		db.Find(&events, "cow_id = ? AND event_type_id IN (1, 2, 3, 4)", id)
+		db.Preload("EventType").Find(&events, "cow_id = ? AND event_type_id IN (1, 2, 3, 4)", id)
 
 		c.JSON(200, events)
 	}
