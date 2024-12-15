5ac1291d9cd430398e8bc9488d650891ce94e559
Popov Dmitriy Ivanovich <PopovDmitriyStudy@yandex.ru>
2024-12-15 18:34:12 +0300

    Add hoz route

--- server/routes/farms/farm.go
+++ server/routes/farms/farm.go
@@ -3,7 +3,7 @@ package farms
 import (
 	"cow_backend/models"
 	"cow_backend/routes/auth"
-	"net/http"
+	// "net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -14,52 +14,19 @@ type Farms struct {
 func (f *Farms) WriteRoutes(rg *gin.RouterGroup) {
 	apiGroup := rg.Group("/farms")
 	authGroup := apiGroup.Group("")
-	authGroup.Use(auth.AuthMiddleware(auth.Farmer, auth.RegionalOff, auth.FederalOff))
-	apiGroup.GET("/:id", f.GetByID())
-	authGroup.GET("/", f.GetByFilter())
 	authGroup.GET("/hoz", func(c *gin.Context) {
-		roleId, exists := c.Get("RoleId")
-		if !exists {
-			c.JSON(http.StatusInternalServerError, "RoleId не найден в контексте")
+		db := models.GetDb()
+		farms := []models.Farm{}
+		
+		if err := db.Find(&farms, map[string]any{"type": 3}).Error; err != nil {
+			c.JSON(500, err.Error())
 			return
 		}
 
-		db := models.GetDb()
-		farms := []models.Farm{}
-		if roleId == 1 {
-			farmId, exists := c.Get("FarmId")
-			if !exists {
-				c.JSON(http.StatusInternalServerError, "FarmId не найден в контексте")
-				return
-			}
-			qres := db.Where("EXISTS (SELECT 1 FROM COWS WHERE cows.farm_group_id = farms.id) AND id = ?", farmId).Find(&farms)
-			if qres.Error != nil {
-				c.JSON(500, qres.Error)
-			}
-
-			c.JSON(200, farms)
-		} else if roleId == 2 {
-			regionId, exists := c.Get("RegionId")
-			if !exists {
-				c.JSON(http.StatusInternalServerError, "RegionId не найден в контексте")
-				return
-			}
-			qres := db.
-				Joins("JOIN districts AS d ON farms.district_id = d.id").
-				Joins("JOIN regions AS r ON r.id = d.region_id").
-				Where("EXISTS (SELECT 1 FROM COWS WHERE cows.farm_group_id = farms.id) AND r.id = ?", regionId).
-				Find(&farms)
-			if qres.Error != nil {
-				c.JSON(500, qres.Error)
-			}
-			c.JSON(200, farms)
-		} else {
-			qres := db.Where("EXISTS (SELECT 1 FROM COWS WHERE cows.farm_group_id = farms.id)").Find(&farms)
-			if qres.Error != nil {
-				c.JSON(500, qres.Error)
-			}
-			c.JSON(200, farms)
-		}
-
+		c.JSON(200, farms)
 	})
+	authGroup.Use(auth.AuthMiddleware(auth.Farmer, auth.RegionalOff, auth.FederalOff))
+	apiGroup.GET("/:id", f.GetByID())
+	authGroup.GET("/", f.GetByFilter())
+	
 }
