49d94ec48dd0640f050feaa29330035f861aa943
Dmitriy <popov.dmitriy.ivanovich@gmail.com>
2024-12-17 17:56:17 +0300

    Add event types tables

--- server/models/events.go
+++ server/models/events.go
@@ -5,17 +5,17 @@ type Event struct {
 
 	CowId uint
 
-	EventType   	EventType
-	EventTypeId 	uint
+	EventType   EventType // стандартизированная группа события
+	EventTypeId uint
 
-	EventType1 		EventType
-	EventType1Id 	uint
+	EventType1   EventType // стандартизированная название события
+	EventType1Id uint
 
-	EventType2 		EventType
-	EventType2Id 	uint
+	EventType2   *EventType // стандартизированное разновидность события
+	EventType2Id *uint
 
-	DataResourse      string // источник данные
-	DaysFromLactation uint   // дни от начала лактации
+	DataResourse      *string // источник данные
+	DaysFromLactation uint    // дни от начала лактации
 
 	Date     DateOnly
 	Comment1 *string
@@ -28,8 +28,8 @@ type EventType struct { // бывший EventList
 	Parent   *EventType `json:"-"`
 	ParentId *uint
 
-	Name string
+	Name string // имя этой штуки
 
-	Code uint
-	Type uint // Should be enum
+	Code uint // код группы или разновидности или события
+	Type uint // 1 - группа события, 2 - разновидность события, 3 - название события
 }
