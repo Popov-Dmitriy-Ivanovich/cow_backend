6e8521320b823a84456b4513db12aa0fab74aca4
Popov-Dmitriy-Ivanovich <popov.dmitriy.ivanovich@gmail.com>
2025-02-03 14:02:47 +0300

    Add cow ids to statistic

--- server/models/statistics.go
+++ server/models/statistics.go
@@ -1,5 +1,7 @@
 package models
 
+import "github.com/lib/pq"
+
 type GaussianStatistics struct {
 	ID uint
 
@@ -9,15 +11,15 @@ type GaussianStatistics struct {
 	Region   *Region `constraint:"OnUpdate:CASCADE,OnDelete:CASCADE;"`
 	RegionID *uint   `gorm:"index"` // не null, если статистика собрана по региону
 
-	MinIndex float64 // Значение минимального индекса
-	MinCount uint    // Количество коров с минимальным индексом
-	//MinCowIds pq.Int64Array `gorm:"type:integer[]"` // ID коров с минимальным значением индекса
+	MinIndex  float64       // Значение минимального индекса
+	MinCount  uint          // Количество коров с минимальным индексом
+	MinCowIds pq.Int64Array `gorm:"type:bigint[]"` // ID коров с минимальным значением индекса
 
-	AvgIndex float64 // Значение среднего индекса
-	AvgCount uint    // Количество коров с минимальным индексом
-	//AvgCowIds pq.Int64Array `gorm:"type:integer[]"` // ID коров с средним значением индекса
+	AvgIndex  float64       // Значение среднего индекса
+	AvgCount  uint          // Количество коров с минимальным индексом
+	AvgCowIds pq.Int64Array `gorm:"type:bigint[]"` // ID коров с средним значением индекса
 
-	MaxIndex float64 // Значение максимального индекса
-	MaxCount uint    // Количество коров с максимальным индексом
-	//MaxCowIds pq.Int64Array `gorm:"type:integer[]"` // ID коров с максимальным значением индекса
+	MaxIndex  float64       // Значение максимального индекса
+	MaxCount  uint          // Количество коров с максимальным индексом
+	MaxCowIds pq.Int64Array `gorm:"type:bigint[]"` // ID коров с максимальным значением индекса
 }
